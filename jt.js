879a81d83fafc6df39216e3364d72693caa97ab1
clm110 <275298711@qq.com>
2018-01-14 22:56:22 +0800

    箭头函数的this定义之后就不会变化了，funciton是可以改变的

--- jt.js
+++ jt.js
@@ -1,17 +1,19 @@
 exports.a= 100
 console.log(this)
-console.log(global)
-// var foo = ()=>{
-    // 指向本文件的module.exports,而且以后都不会变
-//     console.log(this)
-// }
-// foo();
+// console.log(global)
+var cc ={}
 
-// var bar = function(){
-    // this指向global,会变化
+// var foo = ()=>{
+//     //  //指向本文件的module.exports,而且以后都不会变
 //     console.log(this)
 // }
-// bar()
+// // foo.call(cc);
+
+var bar = function(){
+    // this指向global,会变化
+    console.log(this)
+}
+bar.call(cc)
 
 // var obj = {
 //     fn:foo,
