df0b1e0a75ae0b52cc00939538903b056601221a
clm110 <275298711@qq.com>
2018-02-22 22:59:18 +0800

    评论功能完善

--- vue课程项目/src/main.js
+++ vue课程项目/src/main.js
@@ -5,10 +5,23 @@ import VueRouter from 'vue-router'
 // 1.2 安装路由
 Vue.use(VueRouter)
 
+
+// 导入格式化时间的插件
+import moment from 'moment'
+// 定义全局的过滤器
+Vue.filter('dateFormat', function (dataStr, pattern = "YYYY-MM-DD HH:mm:ss") {
+  return moment(dataStr).format(pattern)
+})
+
 // 2.1 导入 vue-resource
 import VueResource from 'vue-resource'
 // 2.2 安装 vue-resource
 Vue.use(VueResource)
+
+// 设置请求的根路径
+Vue.http.options.root = 'http://vue.studyit.io';
+// 全局设置 post 时候表单数据格式组织形式   application/x-www-form-urlencoded
+Vue.http.options.emulateJSON = true;
 
 
 // 导入 MUI 的样式
@@ -18,10 +31,11 @@ import './lib/mui/css/icons-extra.css'
 
 
 // 按需导入 Mint-UI 中的组件   
-import { Header, Swipe, SwipeItem } from 'mint-ui'
+import { Header, Swipe, SwipeItem,Button } from 'mint-ui'
 Vue.component(Header.name, Header)
 Vue.component(Swipe.name, Swipe)
 Vue.component(SwipeItem.name, SwipeItem)
+Vue.component(Button.name, Button)
 
 
 // 1.3 导入自己的 router.js 路由模块
