2aae5e582a3a309e3976621e229873b9b7a9b687
clm110 <275298711@qq.com>
2018-04-12 23:38:38 +0800

    412

---
+++ keep-live/test.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const urllib = require('urllib');
+
+const keepaliveAgent = new http.Agent({
+  keepAlive: true,
+});
+const server = http.createServer((req, res) => {
+    res.writeHead('200',{
+        "Keep-Alive": timeout=5
+    })
+  res.end('Hello World, ' + req.connection.remotePort);
+});
+server.on('clientError', (err, socket) => {
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+server.listen(3000);
+
+function request() {
+  urllib.request('http://127.0.0.1:3000', {
+    // agent: keepaliveAgent,
+    dataType: 'text',
+  }, (err, data, res) => {
+    if (err) {
+      console.error('error: %s, status: %s', err, res.status, res);
+      throw err;
+    }
+    console.log('status: %s, data: %s', res.status, data, res);
+  });
+}
+
+setInterval(request, 5000);
+request();
